0036ea72612fe90293cba0f6383e127e5f598cc2
Min Raj Basnet <minraz123@gmail.com>
2026-03-30 20:34:23 +0545

    feat(tabs):setup bottom tabs

--- app/(subscriptions)/[id].tsx
+++ app/(subscriptions)/[id].tsx
@@ -1,10 +1,13 @@
+import { useLocalSearchParams } from "expo-router";
 import React from "react";
 import { Text, View } from "react-native";
 
 const SubscriptionDetails = () => {
+  const { id } = useLocalSearchParams<{ id: string }>();
+
   return (
     <View>
-      <Text>SubscriptionDetails</Text>
+      <Text>SubscriptionDetails : {id}</Text>
     </View>
   );
 };
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,14 @@
-import { Text, View } from "react-native";
+import { styled } from "nativewind";
+import { Text } from "react-native";
+import { SafeAreaView as RNSafeAreaView } from "react-native-safe-area-context";
+const SafeAreaView = styled(RNSafeAreaView);
 
 export default function Index() {
   return (
-    <View className="flex-1 items-center justify-center bg-background">
+    <SafeAreaView className="flex-1 items-center justify-center bg-background">
       <Text className="text-xl font-bold text-foreground">
         Welcome to Nativewind!
       </Text>
-    </View>
+    </SafeAreaView>
   );
 }
