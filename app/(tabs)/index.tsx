794cc9cad627e61bd7d40e26a8cc0f757e0c27bb
Min Raj Basnet <minraz123@gmail.com>
2026-03-30 22:19:09 +0545

    chore(index):remove import for safe areavie

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,7 +1,5 @@
-import { styled } from "nativewind";
 import { Text } from "react-native";
-import { SafeAreaView as RNSafeAreaView } from "react-native-safe-area-context";
-const SafeAreaView = styled(RNSafeAreaView);
+// const SafeAreaView = styled(RNSafeAreaView);
 
 export default function Index() {
   return (
